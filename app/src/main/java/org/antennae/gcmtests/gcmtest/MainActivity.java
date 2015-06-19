ea67772974ae362c7bcad1edb3474bfc24d98f5a
nsankaran <nsankaran@ebay.com>
2015-06-18 22:44:26 -0700

    cleaned up the code

--- app/src/main/java/org/antennae/gcmtests/gcmtest/MainActivity.java
+++ app/src/main/java/org/antennae/gcmtests/gcmtest/MainActivity.java
@@ -40,15 +40,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         context = getApplicationContext();
         // GcmWrapper startup
 
-
-//        if( checkPlayServices() ){
-//            Intent intent = new Intent(this, MyGcmIntentService.class);
-//            startService(intent);
-//        }
         GcmWrapper gcmwrapper = new GcmWrapper(context);
         String registrationId = gcmwrapper.getRegId();
 
@@ -77,100 +71,6 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Gets the current registration ID for application on GcmWrapper service, if there
-     * is one.
-     * <p>
-     * If result is empty, the app needs to register.
-     *
-     * @return registration ID, or empty string if there is no existing
-     *         registration ID.
-     */
-    private String getRegistrationId(Context context)
-    {
-        final SharedPreferences prefs = getGcmPreferences(context);
-        String registrationId = prefs.getString(Globals.PREFS_PROPERTY_REG_ID, "");
-        if (registrationId == null || registrationId.equals(""))
-        {
-            Log.i(Globals.TAG, "Registration not found.");
-            return "";
-        }
-        // Check if app was updated; if so, it must clear the registration ID
-        // since the existing regID is not guaranteed to work with the new
-        // app version.
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion)
-        {
-            Log.i(Globals.TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
-
-
-    /**
-     * @return Application's {@code SharedPreferences}.
-     */
-    private SharedPreferences getGcmPreferences(Context context)
-    {
-        // This sample app persists the registration ID in shared preferences,
-        // but how you store the regID in your app is up to you.
-        return getSharedPreferences(Globals.PREFS_NAME, Context.MODE_PRIVATE);
-    }
-
-    /**
-     * Sends the registration ID to the 3rd party server via an upstream
-     * GcmWrapper message. Ideally this would be done via HTTP to guarantee success or failure
-     * immediately, but it would require an HTTP endpoint.
-     */
-    private void sendRegistrationIdToBackend()
-    {
-        Log.d(Globals.TAG, "REGISTER USERID: " + regid);
-        //String name = ((EditText)findViewById(R.id.name_edittext)).getText().toString();
-        new AsyncTask<String, Void, String>()
-        {
-            @Override
-            protected String doInBackground(String... params) {
-
-                String msg = "";
-                try
-                {
-                    Bundle data = new Bundle();
-                    data.putString("name", params[0]);
-                    data.putString("action", "com.antoinecampbell.gcmdemo.REGISTER");
-                    String id = Integer.toString(msgId.incrementAndGet());
-                    gcm.send(Globals.GCM_SENDER_ID + "@gcm.googleapis.com", id, Globals.GCM_TIME_TO_LIVE, data);
-                    msg = "Sent registration";
-                }
-                catch (IOException ex)
-                {
-                    msg = "Error :" + ex.getMessage();
-                }
-                return msg;
-            }
-
-            @Override
-            protected void onPostExecute(String msg)
-            {
-                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-            }
-
-        }.execute("NAME : Nambi");
-    }
-
-    /**
-     * @return Application's version code from the {@code PackageManager}.
-     */
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            // should never happen
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
 
     /**
      * Check the device to make sure it has the Google Play Services APK. If
